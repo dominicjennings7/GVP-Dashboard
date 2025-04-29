d1486853de63022c684f43917399877db051de88
dominicjennings7 <dominicjennings@ymail.com>
2025-04-28 20:50:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 # Display the logo
-st.image("gvp_logo.JPG", width=200)  # Adjust width as necessary
+st.image("gvp_logo.JPG", width=300)  # Adjust width as necessary
 # -----------------------------------------------------------------------------
 # Useful functions
 
